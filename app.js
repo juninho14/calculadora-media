efb2cb525c02993578299b442f7fc9a9b17febdb
juninho14 <142364415+juninho14@users.noreply.github.com>
2023-09-05 21:12:53 -0300

    CALCULADORA MEDIA

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("bem vindo🎮🕹") // comando de saida de infomação 
+
+/*
+Posso fazer um 
+comentario de varias 
+linhas */
+
+console.log("bem vindo🥽")
+
+alert("A media é:")
+
+alert((8+7+9+2)/4)
+
+console.log((8+7+9+2)/4)
